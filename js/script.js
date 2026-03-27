99c663b48a7280882deb43dacf105f4c61d3bcb9
DiniZzS2 <dinizjpsilva@gmail.com>
2026-03-27 20:21:04 -0300

    Ultimas alterações no Front-end

--- js/script.js
+++ js/script.js
@@ -1,3 +1,43 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    
+    const menuToggle = document.getElementById('menu-toggle');
+    const navLinks = document.getElementById('nav-links');
+
+    if (menuToggle && navLinks) {
+        menuToggle.addEventListener('click', () => {
+          
+            navLinks.classList.toggle('active');
+            
+            
+            if (navLinks.classList.contains('active')) {
+                menuToggle.innerHTML = '✕';
+                menuToggle.style.color = 'var(--cor-secundaria)';  
+            } else {
+                menuToggle.innerHTML = '☰';
+                menuToggle.style.color = 'var(--cor-texto)';
+            }
+        });
+
+        
+        navLinks.querySelectorAll('a').forEach(link => {
+            link.addEventListener('click', () => {
+                navLinks.classList.remove('active');
+                menuToggle.innerHTML = '☰';
+                menuToggle.style.color = 'var(--cor-texto)';
+            });
+        });
+    }
+
+   
+});
+
+
+
+
+
+
+
 
 const btnTema = document.getElementById('btn-tema');
 const body = document.body;
